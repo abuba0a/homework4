2f74cd8a6a19ff38de8c5706743f83b867dc20cf
abuba0a <168736244+abuba0a@users.noreply.github.com>
2024-05-04 18:15:00 +0300

    Add files via upload

--- homework4.py
+++ homework4.py
@@ -1,1 +1,16 @@
-
+tuple_=(67,'Груша',[87,'DOG',4,25],True,1)
+immutable_var = tuple_
+print(immutable_var)
+tuple_[2][1]='CAT'
+print('immutable_list: ',immutable_var)
+print()
+list_=[67,'Груша',87,'DOG',True,1]
+mutable_list=list_
+print(mutable_list)
+list_[1]='Яблоко'
+print(mutable_list)
+list_.extend('CAT')
+print(mutable_list)
+list_.remove(True)
+list_.extend(['CAT'])
+print('mutable_list: ',mutable_list)
