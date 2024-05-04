0b5d6b80fea7ea3e7892bb35fbcdad65fb1b8a6a
abuba0a <168736244+abuba0a@users.noreply.github.com>
2024-05-04 18:10:56 +0300

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -1,16 +1,1 @@
-tuple_=(67,'Груша',[87,'DOG',4,25],True,1)
-immutable_var = tuple_
-print(immutable_var)
-tuple_[2][1]='CAT'
-print('immutable_list: ',immutable_var)
-print()
-food=[67,'Груша',87,'DOG',True,1]
-mutable_list=food
-print(mutable_list)
-food[1]='Яблоко'
-print(mutable_list)
-food.extend('CAT')
-print(mutable_list)
-food.remove(True)
-food.extend(['CAT'])
-print('mutable_list: ',mutable_list)
+
